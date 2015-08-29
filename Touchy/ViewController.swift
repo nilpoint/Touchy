4c8d9b2639a8b4d8ef85446a0c2a718fd9afa677
Nil Point <nilpoint@hotmail.com>
2015-08-30 06:48:46 +0800

    Initial Commit

---
+++ Touchy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touchy
+//
+//  Created by John Alstru on 8/30/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
