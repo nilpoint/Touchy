a6f7a1ee57e4e78b7735c0b9228779b46febb28a
Nil Point <nilpoint@hotmail.com>
2015-08-30 07:00:55 +0800

    Add a empty TouchyView class.

---
+++ Touchy/TouchyView.swift
@@ -0,0 +1,13 @@
+//
+//  TouchyView.swift
+//  Touchy
+//
+//  Created by John Alstru on 8/30/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import UIKit
+
+class TouchyView: UIView {
+  
+}
